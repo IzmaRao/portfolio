2db7367cea88013e2899fb198e27206ac133d3e6
Izma ikhlaque <147949671+IzmaRao@users.noreply.github.com>
2025-05-13 22:02:31 +0500

    Update page.tsx

--- src/app/(Auth)/about/page.tsx
+++ src/app/(Auth)/about/page.tsx
@@ -7,7 +7,7 @@ function about() {
     <div data-aos="fade-up" className='about-div'>
       <h3 data-aos="fade-up" className='div-about-h'>About Us</h3>
       <p data-aos="fade-up" className='div-about-p'>
-      I&apos;m Izma, a dedicated Full Stack Developer with a strong foundation in web development technologies. My expertise spans HTML, CSS, TypeScript, Next.js, Tailwind CSS, and JavaScript. Over the years, I have honed my skills in crafting responsive, user-friendly websites and applications that offer seamless experiences across all devices. I pride myself on my ability to merge clean, elegant design with efficient, well-structured code, ensuring that every project I work on not only looks great but also performs flawlessly. My passion for web development drives me to continually seek out new challenges and opportunities to learn and grow. Whether it&apos;s building a complex web application from scratch or optimizing an existing project for better performance, I am committed to delivering top-notch solutions that meet and exceed expectations.
+      I&apos;m Izma, a dedicated Full Stack Developer with a strong foundation in web development technologies. My expertise spans HTML, CSS, TypeScript, Next.js, Tailwind CSS, Python and JavaScript. Over the years, I have honed my skills in crafting responsive, user-friendly websites and applications that offer seamless experiences across all devices. I pride myself on my ability to merge clean, elegant design with efficient, well-structured code, ensuring that every project I work on not only looks great but also performs flawlessly. My passion for web development drives me to continually seek out new challenges and opportunities to learn and grow. Whether it&apos;s building a complex web application from scratch or optimizing an existing project for better performance, I am committed to delivering top-notch solutions that meet and exceed expectations.
       </p>
     </div>
     {/* about end */}
@@ -15,4 +15,4 @@ function about() {
   )
 }
 
-export default about
+export default about;
